4bb6f38593dbf2413c4165a961c61e0b259e8e48
Karthikeyan <karthikeyan_u@hotmail.com>
2018-10-26 16:17:06 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+//MIDDLEWARE//
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+//ROUTING//
+app.get('/',function(req,res){
+    res.send('This is the Home Page');
+});
+
+app.get('/campgrounds',function(req,res){
+    res.send('Welcome to Campgrounds Page')
+});
+
+
+
+//SERVER LISTENING//
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log('Server Started');
+});
